aff4b4e58a4be45a72d9cca163f8b145c671f842
Alejandro <kano.verapad@gmail.com>
2021-12-12 22:56:00 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36ee94d44fdfed82c1022664203c2f9f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8516b97a-0165-42a7-9f7e-4697a4dd6095'
+};
